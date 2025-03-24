e0a360f2986e78dea7e0beecbfd942175302ce39
vladislav <vlad.strebkov.00@mail.ru>
2025-03-24 19:53:54 +0300

    feat(core): добавлен метод получения пользователя

--- core/models/user.py
+++ core/models/user.py
@@ -1,9 +1,10 @@
 import datetime
 
-from sqlalchemy import BIGINT, String, Boolean, Integer
+from sqlalchemy import BIGINT, String, Boolean, Integer, select
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Mapped, mapped_column
 
+from core.async_session import async_session
 from core.base import BaseModel
 
 
@@ -22,4 +23,14 @@ class User(BaseModel):
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
     money: Mapped[int] = mapped_column(Integer, default=100, server_default='100', nullable=False)
     get_bonus: Mapped[bool] = mapped_column(Boolean, default=False, server_default='false', nullable=False)
+
+
+
+    @staticmethod
+    async def get_user(user_id: int) -> "User":
+        async with async_session() as session:
+            result = await session.execute(
+                select(User).where(User.id == user_id)
+            )
+            return result.scalars().first()
 
