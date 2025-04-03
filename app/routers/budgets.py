cb4928ce870025dff968d22b508e8395404978e3
vladislav <dorchinets.ira@mail.ru>
2025-04-03 23:41:31 +0300

    feat(app): добавлены эндпоинты получения списка бюджетов, удаления бюджета

--- app/routers/budgets.py
+++ app/routers/budgets.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from types import NoneType
+
+from fastapi import APIRouter, HTTPException
 from core.models.budget import Budget
 
 from app.schemas.budgets import BudgetSchema, BudgetResponseSchema
@@ -17,3 +19,18 @@ async def add_budget(data: BudgetSchema):
         currency_id=data.currency_id
     )
     return await budget.add()
+
+
+
+@router.get("/list/{user_id}", response_model=list[BudgetResponseSchema])
+async def get_list(user_id: int):
+    return await Budget.get_list(user_id=user_id)
+
+
+@router.delete("/{budget_id}", response_model=NoneType)
+async def delete_budget(budget_id: int):
+    budget = await Budget.get_by_id(budget_id=budget_id)
+    if budget:
+        await budget.delete()
+    else:
+        raise HTTPException(status_code=404, detail="Budget not found")
