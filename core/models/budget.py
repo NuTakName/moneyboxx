a7f91d84deff7748229c957516fe3d111f7c4458
vladislav <dorchinets.ira@mail.ru>
2025-04-03 23:42:08 +0300

    feat(core): добавлены методы получения списка бюджетов, получения бюджета по идентификатору

--- core/models/budget.py
+++ core/models/budget.py
@@ -2,9 +2,10 @@ import datetime
 from sqlalchemy.dialects import postgresql
 
 from core.base import BaseModel
+from core.async_session import async_session
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, BIGINT, String, ForeignKey
+from sqlalchemy import Integer, BIGINT, String, ForeignKey, select, desc
 
 
 class Budget(BaseModel):
@@ -24,3 +25,21 @@ class Budget(BaseModel):
     created: Mapped[datetime] = mapped_column(
         postgresql.TIMESTAMP(timezone=True), nullable=False, default=datetime.datetime.now()
     )
+
+
+    @staticmethod
+    async def get_list(user_id: int) -> list["Budget"]:
+        async with async_session() as session:
+            result = await session.execute(
+                select(Budget).where(Budget.user_id == user_id).order_by(desc(Budget.created))
+            )
+            return result.scalars().all()
+
+
+    @staticmethod
+    async def get_by_id(budget_id: int) -> "Budget":
+        async with async_session() as session:
+            result = await session.execute(
+                select(Budget).where(Budget.id == budget_id)
+            )
+            return result.scalars().first()
