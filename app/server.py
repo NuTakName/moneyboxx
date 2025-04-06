0e5488d59164896f7c45ef3615021b43d1a093d4
vladislav <dorchinets.ira@mail.ru>
2025-04-06 23:01:44 +0300

    feat(app): подключение обработчиков к приложению

--- app/server.py
+++ app/server.py
@@ -1,10 +1,34 @@
+import sys
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from loguru import logger
+from notifiers.logging import NotificationHandler
 from starlette.middleware.cors import CORSMiddleware
-from app.routers import users, budgets, currency, categories, operations
 
+from app.error_handlers import validation_exception_handler, internal_exception_handler
+from app.routers import users, budgets, currency, categories, operations
+from config import settings
 
 app = FastAPI()
+
+logger.add(
+    sys.stdout,
+    colorize=True,
+    format="<green>{time:HH:mm:ss}</green> | {level} | <level>{message}</level>",
+)
+
+params = {
+    "token": settings["logger"]['logger_telegram_token'],
+    "chat_id": settings["logger"]['logger_telegram_chat_id'],
+}
+
+handler = NotificationHandler("telegram", defaults=params)
+logger.add(handler, level="ERROR", backtrace=True)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(500, internal_exception_handler)
+
 app.include_router(users.router)
 app.include_router(budgets.router)
 app.include_router(currency.router)
